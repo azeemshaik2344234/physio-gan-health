9790796b9f79cc653b8117e9b7c4e52cd7bad08e
azeemshaik2344234 <azeemshaik1234567@gmail.com>
2025-11-07 21:52:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["physio-gan-health.onrender.com"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
